520aac42569cbe49a0a2b9fbf3d597faedf72fd5
Tamera Middlebrooks <105788615+tameramiddlebrooks@users.noreply.github.com>
2024-03-29 18:55:59 -0400

    Update Chart.js

--- pages/Chart.js
+++ pages/Chart.js
@@ -3,9 +3,8 @@ function Chart () {
     <>
     <h1>Full Birth Chart</h1>
     <p className="chart">
-        Now that you've read your horoscope for your Sun Sign, are you interested in diving deeper into Astrology?
-        Cafe Astrology is a reliable, informative website where you can view your entire birth chart, including your moon sign, rising sign, etc.        <a href="https://astro.cafeastrology.com/natal.php"> 
-        <p>Head On Over!</p>
+         Now that you've gotten info on your sun sign, are you interested in diving deeper into your birth chart?
+        Cafe Astrology is a reliable, informative website where you can view your entire birth chart, including your moon, rising, venus, etc.        <a href="https://astro.cafeastrology.com/natal.php"> 
             </a> 
     </p>
     </>
